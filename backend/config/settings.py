08e0ba2beaeaa17c6a08f9822b5580ec17f0e2da
Abrorbek Ismoilov <abror2142@gmail.com>
2024-10-28 16:26:25 +0500

    Organizing folder structure

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -153,7 +153,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTHENTICATION_BACKENDS = [
-
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
